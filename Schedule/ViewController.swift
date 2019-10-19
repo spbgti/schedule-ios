3066353fe61af76aed3f0db70d2ebe33a506297f
Vladislav <Vladislavglumov@list.ru>
2019-10-19 23:02:10 +0300

    Added the label with the time of the exercise to the table cell

--- Schedule/ViewController.swift
+++ Schedule/ViewController.swift
@@ -4,13 +4,12 @@ import UIKit
 class ViewController: UIViewController {
   
   // MARK: - IBOutlets
-  
   @IBOutlet weak var tableView: UITableView!
   @IBOutlet weak var selectedControl: UISegmentedControl!
+  @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
   
   // MARK: - Public var and let
-  
-  let indetifier = "Cell"
+  let identifier = "Cell"
   var array = [Exercise]() {
     didSet {
       DispatchQueue.main.async {
@@ -20,20 +19,21 @@ class ViewController: UIViewController {
   }
   
   // MARK: - Life Cicle
-  
   override func viewDidLoad() {
     super.viewDidLoad()
     
   }
   
   // MARK: - IBActions
-  
   @IBAction func indexChange(_ sender: Any) {
+    activityIndicator.startAnimating()
+    
     switch selectedControl.selectedSegmentIndex {
     case 0:
       ScheduleRequest.shared.getGroups(groupName: "446") { completion in
         DispatchQueue.main.async {
           ScheduleRequest.shared.getExercises(groupId: String(completion[0].groupId), date: "2019-09-22") { completionHandler in
+            self.activityIndicator.stopAnimating()
             self.array = completionHandler
           }
         }
@@ -42,6 +42,7 @@ class ViewController: UIViewController {
       ScheduleRequest.shared.getGroups(groupName: "446") { completion in
         DispatchQueue.main.async {
           ScheduleRequest.shared.getExercises(groupId: String(completion[0].groupId), date: "2019-09-23") { completionHandler in
+            self.activityIndicator.stopAnimating()
             self.array = completionHandler
           }
         }
@@ -49,6 +50,7 @@ class ViewController: UIViewController {
     case 2:
       ScheduleRequest.shared.getSchedules(year: "2019", groupNumber: "446") { completion in
         DispatchQueue.main.async {
+          self.activityIndicator.stopAnimating()
           self.array = completion[0].exercises
         }
       }
@@ -61,6 +63,7 @@ class ViewController: UIViewController {
 
 // MARK: - UITableViewDataSource and UITableViewDelegate
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
+  
   // MARK: - Count of sections
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return array.count
@@ -68,11 +71,35 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
   
   // MARK: - Setting of cell
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: indetifier, for: indexPath)
+    let cell:CustomTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! CustomTableViewCell
     
-    cell.textLabel?.text = array[indexPath.row].name
+    let pair = array[indexPath.row].pair
+    
+    switch pair {
+    case "1":
+      cell.timeLabel?.text = "09:30 - 11:00"
+    case "2":
+      cell.timeLabel?.text = "11:30 - 13:00"
+    case "3":
+      cell.timeLabel?.text = "14:00 - 15:30"
+    case "4":
+      cell.timeLabel?.text = "16:00 - 17:30"
+    default:
+      break
+    }
+    
+    cell.exerciseLabel?.text = array[indexPath.row].name
     
     return cell
   }
+  
+}
+
+// MARK: CustomTableViewCell
+class CustomTableViewCell: UITableViewCell {
+  
+  // Mark: - IBOutlet; items of the table cell
+  @IBOutlet weak var timeLabel: UILabel!
+  @IBOutlet weak var exerciseLabel: UILabel!
   
 }
