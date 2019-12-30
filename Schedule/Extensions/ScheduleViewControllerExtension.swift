0c19b055ee2cd1993b4312aeaad663ee6d5ebdfd
Vladislav <Vladislavglumov@list.ru>
2019-12-30 15:55:03 +0300

    Fixed Schedule TableView: - added exercise 'window'; - correctly display week day section.

--- Schedule/Extensions/ScheduleViewControllerExtension.swift
+++ Schedule/Extensions/ScheduleViewControllerExtension.swift
@@ -9,8 +9,23 @@
 import UIKit
 
 // MARK: - Kind of section
-enum WeekDaySection: Int {
+
+enum WeekDaySection: Int, CaseIterable {
   case monday = 0, tuesday, wednesday, thursday, friday, saturday, sunday
+  
+  static var arrayOfCases: [WeekDaySection] {
+    var arrayOfCases = Array<WeekDaySection>()
+    
+    for value in WeekDaySection.allCases {
+      arrayOfCases.append(value)
+    }
+    
+    return arrayOfCases
+  }
+}
+
+enum NumberOfPair: Int, CaseIterable {
+  case first = 0, second, third, fourth, fifth
 }
 
 // MARK: - UITableViewDataSource
@@ -18,7 +33,11 @@ extension ScheduleViewController: UITableViewDelegate, UITableViewDataSource {
 
   // Count of sections
   func numberOfSections(in tableView: UITableView) -> Int {
-    return sortedExercises.count
+    if segmentedControl.selectedSegmentIndex == 2 {
+      return WeekDaySection.allCases.count
+    } else {
+      return 1
+    }
   }
   
   // Section settings
@@ -26,25 +45,37 @@ extension ScheduleViewController: UITableViewDelegate, UITableViewDataSource {
     let label = UILabel()
     label.font = UIFont.boldSystemFont(ofSize: 22.0)
     
-    let keys = Array(sortedExercises.keys)
-    
-    if let tableSection = keys[section] as? WeekDaySection {
+    if !self.sortedExercises.isEmpty {
+      let numberOfSection = numberOfSections(in: tableView)
+      var tableSection: WeekDaySection
+      
+      if numberOfSection > 1 {
+        let arrayOfWeekDaySectionCases = WeekDaySection.arrayOfCases
+        tableSection = arrayOfWeekDaySectionCases[section]
+      } else {
+        let sortedExerciseKeys =  Array(sortedExercises.keys)
+        tableSection = sortedExerciseKeys[section]
+      }
+      
       switch tableSection {
       case .monday:
-        label.text = "Monday"
+        label.text = "Понедельник"
       case .tuesday:
-        label.text = "Tuesday"
+        label.text = "Вторник"
       case .wednesday:
-        label.text = "Wednesday"
+        label.text = "Среда"
       case .thursday:
-        label.text = "Thursday"
+        label.text = "Четверг"
       case .friday:
-        label.text = "Friday"
+        label.text = "Пятница"
       case .saturday:
-        label.text = "Saturday"
+        label.text = "Суббота"
       case .sunday:
-        label.text = "Sunday"
+        label.text = "Воскресенье"
       }
+    } else {
+      // In first time before request the data
+      print("Data not found")
     }
     
     return label
@@ -52,51 +83,71 @@ extension ScheduleViewController: UITableViewDelegate, UITableViewDataSource {
   
   // Count of cells
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    let keys = Array(sortedExercises.keys)
-    
-    if let tableSection = keys[section] as? WeekDaySection, let exerciseArray = sortedExercises[tableSection] {
-      return exerciseArray.count
-    }
-    
-    return 0
+    // Max count of exercise in day
+    return NumberOfPair.allCases.count
   }
   
   // Cell settings
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ScheduleTableViewCell
+    let numberOfSection = numberOfSections(in: tableView)
+    //let keysOfEnum = Array(NumberOfPair.allCases)
+    let cellRow = indexPath.row
     
-    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
-    let keys = Array(sortedExercises.keys)
+    switch cellRow {
+    case 0:
+      cell.timeLabel?.text = "09:30\n11:00"
+    case 1:
+      cell.timeLabel?.text = "11:30\n13:00"
+    case 2:
+      cell.timeLabel?.text = "14:00\n15:30"
+    case 3:
+      cell.timeLabel?.text = "16:00\n17:30"
+    case 4:
+      cell.timeLabel?.text = "17:45\n19:15"
+    default:
+      break
+    }
     
-    if let tableSection = keys[indexPath.section] as? WeekDaySection, let sortedExercises = sortedExercises[tableSection]?[indexPath.row] {
-      let exercisePair = sortedExercises.pair
-      let exerciseParity = sortedExercises.parity
-      let exerciseName = sortedExercises.name
+    if !sortedExercises.isEmpty {
+      var keys: [WeekDaySection]
+      var weekDaySection: WeekDaySection
+      var arrayOfExercises: [Exercise]
       
-      switch exercisePair {
-      case "1":
-        cell.timeLabel?.text = "09:30\n11:00"
-      case "2":
-        cell.timeLabel?.text = "11:30\n13:00"
-      case "3":
-        cell.timeLabel?.text = "14:00\n15:30"
-      case "4":
-        cell.timeLabel?.text = "16:00\n17:30"
-      case "5":
-        cell.timeLabel?.text = "17:45\n19:15"
-      default:
-        break
+      if numberOfSection == 1 {
+        keys = Array(sortedExercises.keys)
+        weekDaySection = keys[indexPath.section]
+        arrayOfExercises = sortedExercises[weekDaySection]!
+        
+      } else {
+        weekDaySection = WeekDaySection.arrayOfCases[indexPath.section]
+        arrayOfExercises = sortedExercises[weekDaySection]!
       }
       
-      switch exerciseParity {
-      case "1":
-        cell.parityLabelText?.text = "нечет."
-      case "2":
-        cell.parityLabelText?.text = "четн."
-      default:
-        cell.parityLabelText?.text = ""
+      for obj in arrayOfExercises {
+        let exerciseName = obj.name
+        let exercisePair = obj.pair
+        let exerciseParity = obj.parity
+        
+        if exercisePair == String(indexPath.row + 1) {
+          cell.exerciseLabel?.text = exerciseName
+          switch exerciseParity {
+          case "1":
+            cell.parityLabelText?.text = "нечет."
+          case "2":
+            cell.parityLabelText?.text = "четн."
+          default:
+            cell.parityLabelText?.text = ""
+          }
+          break
+        } else {
+          cell.exerciseLabel?.text = "-"
+          cell.parityLabelText?.text = ""
+        }
       }
-      
-      cell.exerciseLabel?.text = exerciseName
+    } else {
+      cell.exerciseLabel?.text = ""
+      cell.parityLabelText?.text = ""
     }
     
     return cell
