1639dd4e722162957ff325d60c1cfbe514d113f8
Vladislav <Vladislavglumov@list.ru>
2020-02-03 22:54:27 +0300

    Created WelcomeViewController programmatically

--- Schedule/Controllers/WelcomeViewController.swift
+++ Schedule/Controllers/WelcomeViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstLaunchViewController: UIViewController {
+class WelcomeViewController: UIViewController {
   
   // MARK: Properties
   
@@ -17,16 +17,44 @@ class FirstLaunchViewController: UIViewController {
   // MARK: Life cycle
   
   override func loadView() {
-    self.view = 
+    self.view = WelcomeView()
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.view.backgroundColor = .gray
+    
+    view().welcomeLabel.text = "Welcome to SPbGTI schedule app"
+    view().saveButton.addTarget(self, action: #selector(saveGroupNumber), for: .touchUpInside)
+  }
+  
+  // MARK: Methods
+  
+  func view() -> WelcomeView {
+    return self.view as! WelcomeView
   }
  
-  
-//  @IBAction func saveGroupName(_ sender: UIButton) {
-//    let groupName = groupNameLabel.text!
-//    let isFirstLaunchApp = true
-//
-//    userDefaults?.set(groupName, forKey: "groupNameKey")
-//    userDefaults?.set(isFirstLaunchApp, forKey: "isLaunchedBefore")
-//  }
+  @objc func saveGroupNumber() {
+    if let groupNumber = view().groupNumberTextField.text {
+      APIManager.shared.getGroups(groupNumber: groupNumber) { completion in
+        DispatchQueue.main.async {
+          switch completion{
+          case .success(let result):
+            self.userDefaults?.set(result[0].groupId, forKey: "GROUP_ID")
+            self.userDefaults?.set(result[0].number, forKey: "GROUP_NUMBER")
+            self.userDefaults?.set(true, forKey: "IS_LAUNCHED_BEFORE")
+            AppDelegate.shared.rootViewController.switchToScheduleScreen()
+          case .failure(let error):
+            Alert.showBasicAlert(on: self, message: "\(error)", with: "Group not found")
+          }
+        }
+      }
+    } else {
+      DispatchQueue.main.async {
+        Alert.showBasicAlert(on: self, message: "Error", with: "Text field is empty. Enter your group number")
+      }
+    }
+  }
 
 }
