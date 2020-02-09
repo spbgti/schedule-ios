c9be60c8e9041b3c45732bc5daa3cad1017886fc
Vladislav <Vladislavglumov@list.ru>
2020-02-10 00:52:59 +0300

    Created rootViewController setting

--- Schedule/Extensions/AppDelegateExtension.swift
+++ Schedule/Extensions/AppDelegateExtension.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2020 mac. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension AppDelegate {
+  static var shared: AppDelegate {
+    return UIApplication.shared.delegate as! AppDelegate
+  }
+  
+  var rootViewController: RootViewController {
+    return window!.rootViewController as! RootViewController
+  }
+  
+}
