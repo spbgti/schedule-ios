250e148c8b5e8d9a6251054cf47f698a6a85e512
Vladislav <Vladislavglumov@list.ru>
2020-02-17 23:13:52 +0300

    Fix layout and functions to show modal view and return to notification setting

--- Schedule/Controllers/NotificationSettingTableViewController.swift
+++ Schedule/Controllers/NotificationSettingTableViewController.swift
@@ -11,10 +11,6 @@ import UserNotifications
 
 class NotificationSettingTableViewController: UITableViewController {
   
-  // MARK: Properties
-  
-  private let notifications = NotificationManager()
-  
   // MARK: Life cycle
   
   override func viewDidLoad() {
@@ -22,51 +18,97 @@ class NotificationSettingTableViewController: UITableViewController {
     
     title = "Notification"
     
+    tableView = UITableView.init(frame: .zero, style: .grouped)
     tableView.register(NotificationSettingTableViewCell.self, forCellReuseIdentifier: "NotificationCell")
+    tableView.estimatedRowHeight = 64
+    tableView.rowHeight = UITableView.automaticDimension
+    
+    DispatchQueue.main.async {
+      self.tableView.reloadData()
+    }
   }
+  
+  // MARK: Table view data source
   
   override func numberOfSections(in tableView: UITableView) -> Int {
     return 1
   }
   
   override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-    return "Reminder"
+    switch section {
+    case 0:
+      return "Day reminders"
+    default: fatalError("Unknown number of section")
+    }
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 2
+    switch section {
+    case 0:
+      return 2
+    default: fatalError("Unknown section")
+    }
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "NotificationCell", for: indexPath) as! NotificationSettingTableViewCell
+    cell.accessoryType = .disclosureIndicator
     
-    // FIXME: Delete prototype and create working cell
-    switch indexPath.row {
+    switch indexPath.section {
     case 0:
-      cell.nameLabel.text = "In morning"
-      cell.timeLabel.text = "00:00"
-      cell.switchControl.isOn = false
-    case 1:
-      cell.nameLabel.text = "In evening"
-      cell.timeLabel.text = "00:00"
-      cell.switchControl.isOn = false
-    default:
-      break
+      switch indexPath.row {
+      case 0:
+        guard let time = UserDefaultsManager.shared.getObject(forKey: "TIME_MORNING"),
+          let switchValue = UserDefaultsManager.shared.getObject(forKey: "SWITCH_MORNING")
+        else {
+          cell.timeLabel.text = "Unknown time"
+          cell.conditionLabel.text = "off"
+          return cell
+        }
+        
+        cell.timeLabel.text = "Morning at \(time)"
+        cell.conditionLabel.text = "\(switchValue)"
+      case 1:
+       guard let time = UserDefaultsManager.shared.getObject(forKey: "TIME_EVENING"),
+         let switchValue = UserDefaultsManager.shared.getObject(forKey: "SWITCH_EVENING")
+       else {
+         cell.timeLabel.text = "Unknown time"
+         cell.conditionLabel.text = "off"
+         return cell
+       }
+        
+        cell.timeLabel.text = "Evening at \(time)"
+        cell.conditionLabel.text = "\(switchValue)"
+      default: fatalError("Unknown row")
+      }
+    default: fatalError("Unknown section")
     }
     
     return cell
   }
   
+  // MARK: Table view delegate
+  
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    showModalView(ReminderOptionsViewController())
+    
+    switch indexPath.row {
+    case 0:
+      let reminderViewController = ReminderOptionsViewController(pickerDataSource: ["07:00", "08:00", "09:00"])
+      showModalView(reminderViewController)
+    case 1:
+      let reminderViewController = ReminderOptionsViewController(pickerDataSource: ["20:00", "21:00", "22:00"])
+      showModalView(reminderViewController)
+    default: fatalError("")
+    }
   }
   
   // MARK: Methods
   
   func showModalView(_ viewController: UIViewController) {
-    viewController.modalTransitionStyle   = .coverVertical
-    viewController.modalPresentationStyle = .currentContext
-    self.present(viewController, animated: true, completion: nil)
+    let navigationController = UINavigationController(rootViewController: viewController)
+    navigationController.modalTransitionStyle   = .coverVertical
+    navigationController.modalPresentationStyle = .pageSheet
+    self.present(navigationController, animated: true)
   }
   
 }
--- Schedule/Controllers/ReminderOptionsViewController.swift
+++ Schedule/Controllers/ReminderOptionsViewController.swift
@@ -11,52 +11,39 @@ import PureLayout
 
 class ReminderOptionsViewController: UITableViewController {
   
-  // MARK: TableViewCell
-  
-  lazy var datePickerCell: UITableViewCell = {
-    let cell = UITableViewCell()
-    cell.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
-    return cell
-  }()
-  
-  lazy var repeatCell: UITableViewCell = {
-    let cell = UITableViewCell()
-    cell.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
-    cell.textLabel?.text = "Repeat"
-    return cell
-  }()
-  
-  lazy var switchCell: UITableViewCell = {
-    let cell = UITableViewCell()
-    cell.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
-    cell.textLabel?.text = "Tern on/off"
-    return cell
-  }()
-  
-  // MARK: Table view content
-  
-  lazy var datePicker: UIDatePicker = {
-    let datePicker = UIDatePicker()
-    datePicker.timeZone = NSTimeZone.local
-    datePicker.backgroundColor = .white
-    return datePicker
-  }()
-  
-  lazy var switchView: UISwitch = {
-    let switchView = UISwitch()
-    return switchView
-  }()
+  private var pickerDataSource: [String]
+  private let pickerCell = PickerTableViewCell()
+  private let activityCell = SwitchTableViewCell()
   
   // MARK: Life cycle
-
+  
+  init(pickerDataSource: [String]) {
+    self.pickerDataSource = pickerDataSource
+    
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    tableView = UITableView.init(frame: .zero, style: .grouped)
+    title = "Edit reminder"
+    navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(close))
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(save))
     
-    datePickerCell.addSubview(datePicker)
-    repeatCell.addSubview(switchView)
-    switchCell.addSubview(switchView)
+    pickerCell.pickerView.delegate = self
+    pickerCell.pickerView.dataSource = self
+    
+    tableView = UITableView.init(frame: .zero, style: .grouped)
+    tableView.estimatedRowHeight = 80
+    tableView.rowHeight = UITableView.automaticDimension
+    
+    DispatchQueue.main.async {
+      self.tableView.reloadData()
+    }
   }
 
   // MARK: Table view data source
@@ -64,13 +51,13 @@ class ReminderOptionsViewController: UITableViewController {
   override func numberOfSections(in tableView: UITableView) -> Int {
       return 2
   }
-
+  
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     switch section {
     case 0:
       return 1
     case 1:
-      return 2
+      return 1
     default: fatalError("Unknown number of section")
     }
   }
@@ -78,17 +65,42 @@ class ReminderOptionsViewController: UITableViewController {
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     switch indexPath.section {
     case 0:
-      return self.datePickerCell
+      return pickerCell
     case 1:
       switch indexPath.row {
       case 0:
-        return self.repeatCell
-      case 1:
-        return self.switchCell
+        activityCell.textLabel?.text = "Turn off/on"
+        return activityCell
       default: fatalError("Unknown row")
       }
     default: fatalError("Unknown section")
     }
   }
+  
+  // MARK: @objc methods
 
+  @objc func close() {
+    self.dismiss(animated: true, completion: nil)
+  }
+  
+  @objc func save() {
+    // TODO: add or update notification
+    close()
+  }
+
+}
+
+extension ReminderOptionsViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return pickerDataSource.count
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return pickerDataSource[row]
+  }
+  
 }
