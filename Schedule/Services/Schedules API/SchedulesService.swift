817b53da6841fadd1565b0c0c1a0a373826e4631
vladislav <vladislavglumov@list.ru>
2020-09-30 14:12:06 +0300

    Create schedules service
    - create schedules service with one method to get all schedules by year, education semester and group number;
    - create enum of eduction semester.

---
+++ Schedule/Services/Schedules API/SchedulesService.swift
@@ -0,0 +1,40 @@
+//
+//  SchedulesService.swift
+//  schedule
+//
+//  Created by vladislav on 30.09.2020.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import Foundation
+
+enum AcademicSemester: String {
+    case fall = "1"
+    case spring = "2"
+}
+
+class SchedulesService {
+    
+    private let provider = NetworkProvider<SchedulesEndpoint>()
+    
+    func getSchedules(year: Date, semester: AcademicSemester, groupNumber: String, completion: @escaping (Result<[Schedule]>) -> Void) {
+        let yearString = dateFormatter(year)
+        let semesterString = semester.rawValue
+        
+        provider.request(.get(year: yearString, semester: semesterString, groupNumber: groupNumber)) { (result: Result<[Schedule]>) in
+            switch result {
+            case .success(let schedules):
+                completion(.success(schedules))
+            case .failure(let errorString):
+                completion(.failure(errorString))
+            }
+        }
+    }
+    
+    private func dateFormatter(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy"
+        return formatter.string(from: date)
+    }
+    
+}
