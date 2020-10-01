e56300d363afec37518d2bac87827e9f19d59dfc
vladislav <vladislavglumov@list.ru>
2020-10-01 14:59:17 +0300

    Create Main module
    - create Main module without references to other storyboard;
    - relocate groups of views and view controllers to group of modules.

---
+++ Schedule/Modules/Main/MainController.swift
@@ -0,0 +1,19 @@
+//
+//  MainController.swift
+//  schedule
+//
+//  Created by vladislav on 01.10.2020.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.selectedIndex = 1
+    }
+
+}
